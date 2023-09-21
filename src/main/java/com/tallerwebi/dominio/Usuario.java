2fa618747776d7b17c41969bd33e4cbf2ad8120e
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2023-09-21 17:03:49 -0300

    se completo la entidad usuario

--- src/main/java/com/tallerwebi/dominio/Usuario.java
+++ src/main/java/com/tallerwebi/dominio/Usuario.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Usuario {
@@ -11,10 +12,14 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String nombre;
+    private String apellido;
     private String email;
     private String password;
     private String rol;
     private Boolean activo = false;
+    private Date created_at;
+    private Date updated_at;
 
     public Long getId() {
 
@@ -55,4 +60,38 @@ public class Usuario {
     public void activar() {
         activo = true;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
 }
