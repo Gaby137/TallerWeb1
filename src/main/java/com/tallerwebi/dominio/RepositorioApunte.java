d90fe1625edb8d040595859f32eb58de9e0fff72
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2023-09-28 16:42:03 -0300

    la vista misApuntes ahora es dinamica y se modifico el encabezado

--- src/main/java/com/tallerwebi/dominio/RepositorioApunte.java
+++ src/main/java/com/tallerwebi/dominio/RepositorioApunte.java
@@ -1,9 +1,12 @@
 package com.tallerwebi.dominio;
 
+import java.util.List;
+
 public interface RepositorioApunte {
 
     void registrarApunte(Apunte apunte);
     Apunte obtenerApunte(Long id);
     void modificarApunte(Apunte apunte);
     void eliminarApunte(Apunte apunte);
+    List<Apunte> obtenerApuntes();
 }
