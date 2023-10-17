7018d0670d45d0c2e45aed69381cc70d1067cd1c
tomycernik <cerniktomy@gmail.com>
2023-10-16 21:45:53 -0300

    Se visualizan los apuntes subidos por otros usuarios en vista "apuntes"

--- src/main/java/com/tallerwebi/presentacion/ControladorResena.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorResena.java
@@ -1,10 +1,13 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.entidad.Apunte;
 import com.tallerwebi.dominio.entidad.Resena;
 import com.tallerwebi.dominio.entidad.Usuario;
 import com.tallerwebi.dominio.entidad.UsuarioApunteResena;
+import com.tallerwebi.dominio.servicio.ServicioApunte;
 import com.tallerwebi.dominio.servicio.ServicioResena;
 import com.tallerwebi.dominio.servicio.ServicioUsuario;
+import com.tallerwebi.dominio.servicio.ServicioUsuarioApunteResena;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -22,17 +28,22 @@ public class ControladorResena {
 
     private ServicioResena servicioResena;
     private ServicioUsuario servicioUsuario;
+    private ServicioApunte servicioApunte;
+    private ServicioUsuarioApunteResena servicioUsuarioApunteResena;
 
     @Autowired
-    public ControladorResena(ServicioResena servicioResena, ServicioUsuario servicioUsuario) {
+    public ControladorResena(ServicioResena servicioResena, ServicioUsuario servicioUsuario, ServicioApunte servicioApunte, ServicioUsuarioApunteResena servicioUsuarioApunteResena) {
         this.servicioResena = servicioResena;
         this.servicioUsuario = servicioUsuario;
+        this.servicioApunte = servicioApunte;
+        this.servicioUsuarioApunteResena = servicioUsuarioApunteResena;
     }
 
     @RequestMapping(path = "/formulario-alta-resena", method = RequestMethod.GET)
     public ModelAndView irAFormularioAlta(HttpSession session) {
         ModelMap model = new ModelMap();
-        Usuario usuario = (Usuario) session.getAttribute("usuario");
+
+        Usuario usuario=(Usuario) session.getAttribute("usuario");
         model.put("usuario", usuario);
         model.put("resena", new Resena());
         return new ModelAndView("formulario-alta-resena", model);
@@ -40,64 +51,42 @@ public class ControladorResena {
 
     @RequestMapping(path = "/guardarResena", method = RequestMethod.POST)
     public ModelAndView guardarResena(@ModelAttribute("resena") Resena resena, HttpSession session) {
-        ModelMap model = new ModelMap();
+        ModelMap modelo = new ModelMap();
+        Usuario usuario=(Usuario) session.getAttribute("usuario");
+        Long id = (Long) session.getAttribute("idApunte");
+        Apunte apunte = servicioApunte.obtenerPorId(id);
+        modelo.put("id", id);
 
         if (resena != null) {
-            // Obtener el usuario de la sesión
-            Usuario usuario = (Usuario) session.getAttribute("usuario");
-
-            if (usuario != null) {
-                // Verificar si la propiedad usuarioResenaApunte es nula y, si es así, inicializarla
-                if (resena.getUsuarioResenaApunte() == null) {
-                    resena.setUsuarioResenaApunte(new UsuarioApunteResena());
-                }
-
-                // Asignar el usuario a la reseña
-                resena.getUsuarioResenaApunte().setUsuario(usuario);
-
-                // Otorgar 10 puntos al usuario
-                usuario.setPuntos(usuario.getPuntos() + 10);
-                servicioUsuario.actualizar(usuario);
-
-                // Guardar la reseña
-                servicioResena.guardar(resena);
-
-                return listarResenas();
-            } else {
-                model.put("mensaje", "Usuario asociado a la reseña es nulo");
+            if(servicioUsuarioApunteResena.registrar(usuario ,apunte, resena)){
+                return new ModelAndView("redirect:/misApuntes");
+            }else {
+                modelo.put("error", "No puede dar mas de una reseña");
+                return new ModelAndView("formulario-alta-resena", modelo);
             }
-        } else {
-            model.put("mensaje", "Reseña es nula");
+
+
         }
 
-        return new ModelAndView("formulario-alta-resena", model);
-    }
-
-    @RequestMapping(path = "/apunte-detalle", method = RequestMethod.GET)
-    public ModelAndView listarResenas() {
-        ModelMap model = new ModelMap();
-        List<Resena> resenas = servicioResena.listar();
-        model.put("resenas", resenas);
-        return new ModelAndView("apunte-detalle", model);
+        return new ModelAndView("redirect:/detalleApunte");
     }
 
     @RequestMapping(path = "/borrarResena/{id}", method = RequestMethod.GET)
-    public ModelAndView borrar(@PathVariable("id") Long id) {
+    public ModelAndView borrar(@PathVariable("id") Long id, HttpSession session) {
         ModelMap modelo = new ModelMap();
+        Long idApunte = (Long) session.getAttribute("idApunte");
+        modelo.put("id", idApunte);
         try {
-            Resena resena = servicioResena.buscar(id);
+            servicioResena.borrar(id);
+            modelo.put("mensaje", "Reseña borrada exitosamente");
 
-            if (resena != null) {
-                servicioResena.borrar(id);
-                modelo.put("mensaje", "Reseña borrada exitosamente");
-            } else {
-                modelo.put("error", "La reseña con el ID proporcionado no existe");
-            }
         } catch (Exception e) {
             modelo.put("error", "Error al intentar borrar la reseña");
         }
-        return new ModelAndView("redirect:/apunte-detalle", modelo);
+        return new ModelAndView("redirect:/detalleApunte/{id}", modelo);
     }
+
 }
+
 
 
