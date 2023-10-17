e9b3b4d3e385193126f789e17750e4291bcbc641
tomycernik <cerniktomy@gmail.com>
2023-10-16 21:48:06 -0300

    Se visualizan los apuntes subidos por otros usuarios en vista "apuntes"

--- src/main/java/com/tallerwebi/dominio/entidad/Usuario.java
+++ src/main/java/com/tallerwebi/dominio/entidad/Usuario.java
@@ -15,42 +15,26 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "El nombre no puede estar en blanco.")
-    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres.")
-    @Pattern(regexp = "^[a-zA-ZáéíóúüÁÉÍÓÚÜ\\s]*$", message = "El nombre solo puede contener letras y espacios.")
     private String nombre;
-    @NotEmpty(message = "El apellido no puede estar en blanco.")
-    @Size(min = 2, max = 50, message = "El apellido debe tener entre 2 y 50 caracteres.")
-    @Pattern(regexp = "^[a-zA-ZáéíóúüÁÉÍÓÚÜ\\s]*$", message = "El apellido solo puede contener letras y espacios.")
     private String apellido;
     private int puntos;
-    @NotEmpty(message = "El mail no puede estar en blanco.")
-    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@alumno\\.unlam\\.edu\\.ar$", message = "El mail debe ser una de la institución")
     private String email;
-    @NotEmpty(message = "La contraseña no puede estar en blanco.")
-    @Size(min = 6, max = 20, message = "La contraseña debe tener entre 6 y 20 caracteres")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", message = "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un dígito.")
-    @NotBlank
     private String password;
     @Enumerated(EnumType.STRING)
     private Rol rol;
     private Boolean activo = false;
+    private String fotoPerfil;
     private Date created_at;
     private Date updated_at;
-
-    @Version
-    private Long version;
-    @OneToMany(mappedBy = "usuario")
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
     private List<UsuarioApunte> relacionesUsuarioApunte = new ArrayList<>();
 
     @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
     private UsuarioApunteResena usuarioResenaApunte;
 
-    public Usuario(){
+    public Usuario(){}
 
-    }
-    public Usuario(Long id, String nombre, String apellido, int puntos, String email, String password, Rol rol, Boolean activo, Date created_at, Date updated_at, List<UsuarioApunte> relacionesUsuarioApunte, Long version) {
-        this.id = id;
+    public Usuario(String nombre, String apellido, int puntos, String email, String password, Rol rol, Boolean activo, String fotoPerfil, Date created_at, Date updated_at) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.puntos = puntos;
@@ -58,10 +42,9 @@ public class Usuario {
         this.password = password;
         this.rol = rol;
         this.activo = activo;
+        this.fotoPerfil = fotoPerfil;
         this.created_at = created_at;
         this.updated_at = updated_at;
-        this.relacionesUsuarioApunte = relacionesUsuarioApunte;
-        this.version=version;
     }
 
     public Long getId() {
@@ -104,6 +87,14 @@ public class Usuario {
 
     public void activar() {
         activo = true;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
     }
 
     public String getNombre() {
@@ -158,13 +149,5 @@ public class Usuario {
 
     public void setUsuarioResenaApunte(UsuarioApunteResena usuarioResenaApunte) {
         this.usuarioResenaApunte = usuarioResenaApunte;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
     }
 }
--- src/main/java/com/tallerwebi/dominio/servicio/ServicioLoginImpl.java
+++ src/main/java/com/tallerwebi/dominio/servicio/ServicioLoginImpl.java
@@ -56,3 +56,4 @@ public class ServicioLoginImpl implements ServicioLogin {
 
 }
 
+
