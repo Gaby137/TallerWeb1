f413c0200b530c1235663c83ca3cd86199715379
tomycernik <cerniktomy@gmail.com>
2023-11-08 13:21:31 -0300

    Clase RepositorioUsuarioApunteResenaTest

--- src/test/java/com/tallerwebi/infraestructura/RepositorioApunteTest.java
+++ src/test/java/com/tallerwebi/infraestructura/RepositorioApunteTest.java
@@ -31,20 +31,16 @@ public class RepositorioApunteTest {
     @Rollback
     @Test
     public void guardarApunte() {
-        // Crear un nuevo apunte
         Apunte apunte = new Apunte();
         apunte.setNombre("Nombre Apunte");
         apunte.setDescripcion("Descripcion");
         apunte.setPrecio(15);
         apunte.setPathArchivo("Path");
 
-        // Guardar el apunte en la base de datos
         repositorioApunte.registrarApunte(apunte);
 
-        // Buscar por id el apunte recien guardado
         Apunte apunteGuardado = repositorioApunte.obtenerApunte(apunte.getId());
 
-        // Verificar que la reseña guardada sea igual a la reseña original
         assertThat(apunteGuardado, equalTo(apunte));
     }
 
@@ -53,25 +49,20 @@ public class RepositorioApunteTest {
     @Rollback
     @Test
     public void borrarApunte() {
-        // Crear un nuevo apunte
         Apunte apunte = new Apunte();
         apunte.setNombre("Nombre Apunte");
         apunte.setDescripcion("Descripcion");
         apunte.setPrecio(15);
         apunte.setPathArchivo("Path");
 
-        // Guardar el apunte en la base de datos
         repositorioApunte.registrarApunte(apunte);
 
         Long id = apunte.getId();
 
-        // Borrar la reseña de la base de datos
         repositorioApunte.eliminarApunte(apunte);
 
-        // Intentar buscar la reseña borrada por ID
         Apunte apunteBorrado = repositorioApunte.obtenerApunte(id);
 
-        // Verificar que no se encuentre ninguna reseña con ese ID
         assertNull(apunteBorrado, "El apunte debe haber sido borrado correctamente");
     }
 
@@ -79,19 +70,16 @@ public class RepositorioApunteTest {
     @Rollback
     @Test
     public void deberiaDevolverApunteAlBuscarloPorSuId() {
-        // Crear un nuevo apunte
         Apunte apunte = new Apunte();
         apunte.setNombre("Nombre Apunte");
         apunte.setDescripcion("Descripcion");
         apunte.setPrecio(15);
         apunte.setPathArchivo("Path");
 
-        // Guardar el apunte en la base de datos
         repositorioApunte.registrarApunte(apunte);
 
         Apunte apunteObtenido = repositorioApunte.obtenerApunte(apunte.getId());
 
-        // Verificar que se encuentre el apunte
         assertEquals(apunte, apunteObtenido, "El apunte encontrado debe ser igual al apunte original");
         assertEquals(apunte.getId(), apunteObtenido.getId(), "Debe haber exactamente un apunte");
     }
