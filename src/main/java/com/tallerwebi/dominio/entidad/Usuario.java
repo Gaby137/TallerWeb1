d33f749042649622e10ae1bd9db5a0f54097b848
tomycernik <cerniktomy@gmail.com>
2023-11-10 16:37:38 -0300

    Flags para dar puntos por creación de reseñas y apuntes y sus respectivos tests

--- src/main/java/com/tallerwebi/dominio/entidad/Usuario.java
+++ src/main/java/com/tallerwebi/dominio/entidad/Usuario.java
@@ -6,6 +6,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Usuario {
